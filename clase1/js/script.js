5c092203efdc2affd67f6049ff86065064256fcd
Luis Flores <luis.r.f.e@hotmail.com>
2023-11-13 09:22:44 -0600

    clase 2

--- clase1/js/script.js
+++ clase1/js/script.js
@@ -1,1 +1,15 @@
-console.log('Sesion cs07');
+const h1 = document.querySelector('#title')
+function change() {
+    h1.innerHTML = "<strong>Me llamo LL</strong>";
+}
+
+
+
+
+function cambiarcolor(colors) {
+    if (h1.style.color === 'green') {
+        h1.style.color = 'black';
+    } else {
+        h1.style.color = colors
+    }
+}
